70f086d8a3231e5553c83c3316b421b3ee531caf
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-02-19 20:49:31 +0900

    v1.1.11

--- inject.js
+++ inject.js
@@ -1,5 +1,4 @@
 (() => {
-    const app = document.querySelector('ytd-app') ?? document.body;
     const http = new XMLHttpRequest();
     let player;
     let thumbnail;
@@ -16,27 +15,22 @@
     }
 
     document.addEventListener('_tap_thumbnail_show', e => {
-        player = player ?? app.querySelector('div#movie_player');
+        player = player ?? document.querySelector('div#movie_player');
         if (player) {
             const video_id = player.getVideoData()?.video_id;
             if (video_id) {
                 thumbnail = thumbnail ?? player.querySelector('input._tap_thumbnail_button');
-                if (thumbnail) {
-                    if (set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/maxresdefault.jpg`)) {
-                        return;
-                    } else if (set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/sddefault.jpg`)) {
-                        return;
-                    } else if (set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/hqdefault`)) {
-                        return;
-                    } else if (set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/mqdefault.jpg`)) {
-                        return;
-                    } else if (set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/default.jpg`)) {
-                        return;
-                    } else {
-                        set_src(thumbnail, '');
-                    }
-                }
+                thumbnail && (
+                    set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/maxresdefault.jpg`) ||
+                    set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/sddefault.jpg`) ||
+                    set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/hqdefault`) ||
+                    set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/mqdefault.jpg`) ||
+                    set_src(thumbnail, `https://i.ytimg.com/vi/${video_id}/default.jpg`) ||
+                    set_src(thumbnail, '')
+                );
             }
         }
     });
+
+    document.dispatchEvent(new CustomEvent('_tap_thumbnail_init'));
 })();
