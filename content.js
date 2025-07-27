f15274c336b894da9c956b6d1eefb8a427d6b6a3
Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>
2025-07-27 15:23:23 +0900

    new_style

--- content.js
+++ content.js
@@ -1,10 +1,15 @@
 main(document.querySelector('ytd-app') ?? document.body);
 
 function main(app) {
-    function create_thumbnail_button() {
+    function create_thumbnail_button(new_style) {
         const button = document.createElement('button');
         button.classList.add('_tap_thumbnail_button', 'ytp-button');
-        button.innerHTML = '<svg viewBox="0 0 512 512" style="width: 100%; height: 100%;" transform="scale(0.5 0.5)"><g><path d="M0,0v512h512V0H0z M163.15,120.064c28.778,0,52.101,23.331,52.101,52.11s-23.323,52.11-52.101,52.11 c-28.777,0-52.109-23.331-52.109-52.11S134.372,120.064,163.15,120.064z M454.417,386.525c-1.789,3.325-5.267,5.411-9.05,5.411 H66.633c-3.774,0-7.244-2.067-9.041-5.392c-1.798-3.326-1.627-7.37,0.449-10.525l66.248-100.939 c4.394-6.695,11.657-10.965,19.646-11.531c8-0.566,15.792,2.625,21.094,8.637l45.693,51.786l82.793-125.996 c4.745-7.217,12.807-11.576,21.444-11.585c8.637-0.009,16.708,4.332,21.463,11.54L453.95,375.992 C456.034,379.156,456.215,383.2,454.417,386.525z"></path></g></svg>';
+        if (new_style) {
+            button.innerHTML = '<svg viewBox="0 0 512 512" style="width: 50%; height: 50%;"><g><path d="M0,0v512h512V0H0z M163.15,120.064c28.778,0,52.101,23.331,52.101,52.11s-23.323,52.11-52.101,52.11 c-28.777,0-52.109-23.331-52.109-52.11S134.372,120.064,163.15,120.064z M454.417,386.525c-1.789,3.325-5.267,5.411-9.05,5.411 H66.633c-3.774,0-7.244-2.067-9.041-5.392c-1.798-3.326-1.627-7.37,0.449-10.525l66.248-100.939 c4.394-6.695,11.657-10.965,19.646-11.531c8-0.566,15.792,2.625,21.094,8.637l45.693,51.786l82.793-125.996 c4.745-7.217,12.807-11.576,21.444-11.585c8.637-0.009,16.708,4.332,21.463,11.54L453.95,375.992 C456.034,379.156,456.215,383.2,454.417,386.525z"></path></g></svg>';
+
+        } else {
+            button.innerHTML = '<svg viewBox="0 0 512 512" style="width: 100%; height: 100%;" transform="scale(0.5 0.5)"><g><path d="M0,0v512h512V0H0z M163.15,120.064c28.778,0,52.101,23.331,52.101,52.11s-23.323,52.11-52.101,52.11 c-28.777,0-52.109-23.331-52.109-52.11S134.372,120.064,163.15,120.064z M454.417,386.525c-1.789,3.325-5.267,5.411-9.05,5.411 H66.633c-3.774,0-7.244-2.067-9.041-5.392c-1.798-3.326-1.627-7.37,0.449-10.525l66.248-100.939 c4.394-6.695,11.657-10.965,19.646-11.531c8-0.566,15.792,2.625,21.094,8.637l45.693,51.786l82.793-125.996 c4.745-7.217,12.807-11.576,21.444-11.585c8.637-0.009,16.708,4.332,21.463,11.54L453.95,375.992 C456.034,379.156,456.215,383.2,454.417,386.525z"></path></g></svg>';
+        }
         button.addEventListener('click', shortcut_command_show);
         return button;
     }
@@ -74,11 +79,11 @@ function main(app) {
         });
     };
 
-    const thumbnail_button = create_thumbnail_button();
     const thumbnail_container = create_thumbnail_container();
     const thumbnail = create_thumbnail();
 
     let player;
+    let thumbnail_button;
 
     document.body.addEventListener('mouseleave', shortcut_command_hide);
 
@@ -103,6 +108,8 @@ function main(app) {
             }
 
             clearInterval(detect_interval);
+
+            thumbnail_button = create_thumbnail_button(getComputedStyle(area).display === 'flex');
 
             area.insertBefore(thumbnail_button, area.firstChild);
             player.appendChild(thumbnail_container);
