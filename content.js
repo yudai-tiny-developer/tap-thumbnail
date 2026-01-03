2e530874eb37b537826ac6f80ca5fa784c794f6b
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2026-01-04 08:15:25 +0900

    v1.3.2 (#23)
    Co-authored-by: Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 main(document.querySelector('ytd-app') ?? document.body);
 
 function main(app) {
-    function create_thumbnail_button(new_style) {
+    function create_thumbnail_button(delhi) {
         const button = document.createElement('button');
         button.classList.add('_tap_thumbnail_button', 'ytp-button');
-        if (new_style) {
+        if (delhi) {
             button.innerHTML = '<svg viewBox="0 0 512 512" style="width: 50%; height: 50%;"><g><path d="M0,0v512h512V0H0z M163.15,120.064c28.778,0,52.101,23.331,52.101,52.11s-23.323,52.11-52.101,52.11 c-28.777,0-52.109-23.331-52.109-52.11S134.372,120.064,163.15,120.064z M454.417,386.525c-1.789,3.325-5.267,5.411-9.05,5.411 H66.633c-3.774,0-7.244-2.067-9.041-5.392c-1.798-3.326-1.627-7.37,0.449-10.525l66.248-100.939 c4.394-6.695,11.657-10.965,19.646-11.531c8-0.566,15.792,2.625,21.094,8.637l45.693,51.786l82.793-125.996 c4.745-7.217,12.807-11.576,21.444-11.585c8.637-0.009,16.708,4.332,21.463,11.54L453.95,375.992 C456.034,379.156,456.215,383.2,454.417,386.525z"></path></g></svg>';
 
         } else {
@@ -133,7 +133,9 @@ function main(app) {
 
             clearInterval(detect_interval);
 
-            thumbnail_button = create_thumbnail_button(getComputedStyle(area).display === 'flex');
+            const delhi = player.classList.contains('ytp-delhi-modern');
+
+            thumbnail_button = create_thumbnail_button(delhi);
             append_thumbnail_button();
 
             video.addEventListener('canplay', async () => {
